c784deb173e429bbe2b278ca8ff21bcda7f460f3
Talha shiekh <137197166+Talha12Shiekh@users.noreply.github.com>
2023-07-18 00:24:31 -0700

    Image picker using react native

---
+++ src/ImagePicker.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function ImagePickerExample() {
+  const [image, setimage] = useState(null);
+
+  const PickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setimage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={PickImage} />
+      {image ? (
+        <Image
+          source={{
+            uri: image,
+          }}
+          style={{ width: 200, height: 200, marginTop: 10 }}
+        />
+      ) : null}
+    </View>
+  );
+}
